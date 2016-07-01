e4e035d2a641801a124479d4177002b26ce452f1
christopherthielen <github@sandgnat.com>
2016-07-01 10:37:38 -0500

    chore(*): update for ng2 rc.3 and ui-router beta.1

--- ng1-to-ng2.ts
+++ ng1-to-ng2.ts
@@ -2,8 +2,8 @@ import * as angular from "angular";
 import {provide, ElementRef, Component, Inject, ComponentMetadata} from "@angular/core";
 import {UpgradeAdapter} from "@angular/upgrade";
 import {
-    UiView, UIRouter, TransitionService, StateService, UIRouterGlobals, UIROUTER_DIRECTIVES, forEach, extend,
-    UrlRouter, ViewService, StateRegistry, UrlMatcherFactory, Ng2ViewDeclaration, Ng2ViewConfig, Node
+    UIView, UIRouter, TransitionService, StateService, Globals, UIROUTER_DIRECTIVES, forEach, extend,
+    UrlRouter, ViewService, StateRegistry, UrlMatcherFactory, Ng2ViewDeclaration, Ng2ViewConfig, PathNode
 } from "ui-router-ng2";
 import {Ng1ViewConfig} from "angular-ui-router";
 
@@ -13,7 +13,7 @@ export let upgradeModule = angular.module('ui.router.upgrade', ['ui.router']);
   selector: 'ui-view-ng-upgrade',
   template: `<ui-view></ui-view>`,
   directives: [UIROUTER_DIRECTIVES],
-  viewProviders: [ provide(UiView.PARENT_INJECT, {useValue: { } }) ],
+  viewProviders: [ provide(UIView.PARENT_INJECT, {useValue: { } }) ],
 })
 /**
  * This angular 2 Component exposes the parent ViewContext as an ng2 DI Provider.
@@ -21,8 +21,8 @@ export let upgradeModule = angular.module('ui.router.upgrade', ['ui.router']);
  * It gets the parent ViewContext information (from the parent ng1 ui-view) by walking
  * up the DOM and grabbing the .data('$uiView') that the ng1 ui-view directive provided.
  */
-class UiViewNgUpgrade {
-  constructor(ref: ElementRef, @Inject(UiView.PARENT_INJECT) parent, registry: StateRegistry) {
+class UIViewNgUpgrade {
+  constructor(ref: ElementRef, @Inject(UIView.PARENT_INJECT) parent, registry: StateRegistry) {
     // From the ui-view-ng-upgrade component's element ref, walk up the DOM two elements...
     // There will first be one ng1 ui-view which hosts this element, and then that ui-view's
     // parent element.  This element has access to the proper "parent viewcontext"
@@ -54,7 +54,7 @@ export let uiRouterNgUpgrade = {
   setUpgradeAdapter(upgradeAdapter: UpgradeAdapter) {
     // Downgrade the ng2 Component to an ng1 directive, to be used in a (generated) view
     // template by ui-router, whenever it finds a view with a `component: Ng2ComponentClass`
-    upgradeModule.directive("uiViewNgUpgrade", <any> upgradeAdapter.downgradeNg2Component(UiViewNgUpgrade));
+    upgradeModule.directive("uiViewNgUpgrade", <any> upgradeAdapter.downgradeNg2Component(UIViewNgUpgrade));
 
     // Register the ng1 DI UIRouter instance as an ng2 Provider
     upgradeAdapter.upgradeNg1Provider('ng1UIRouter', {asToken: UIRouter});
@@ -65,7 +65,7 @@ export let uiRouterNgUpgrade = {
     upgradeAdapter.addProvider(provide(UrlRouter,         { useFactory: (r: UIRouter) => { return r.urlRouter; }, deps: [UIRouter]}));
     upgradeAdapter.addProvider(provide(ViewService,       { useFactory: (r: UIRouter) => { return r.viewService; }, deps: [UIRouter]}));
     upgradeAdapter.addProvider(provide(StateRegistry,     { useFactory: (r: UIRouter) => { return r.stateRegistry; }, deps: [UIRouter]}));
-    upgradeAdapter.addProvider(provide(UIRouterGlobals,   { useFactory: (r: UIRouter) => { return r.globals; }, deps: [UIRouter]}));
+    upgradeAdapter.addProvider(provide(Globals,   { useFactory: (r: UIRouter) => { return <Globals> r.globals; }, deps: [UIRouter]}));
     
   }
 };
@@ -109,9 +109,9 @@ upgradeModule.config([ '$stateProvider', $stateProvider => {
 }]);
 
 upgradeModule.run([ '$view', $view => {
-  $view.viewConfigFactory('ng2', (node: Node, config: Ng2ViewDeclaration) => new Ng2ViewConfig(node, config));
+  $view.viewConfigFactory('ng2', (path: PathNode[], config: Ng2ViewDeclaration) => new Ng2ViewConfig(path, config));
 
-  $view.viewConfigFactory('ng1-to-ng2', (node: Node, config: Ng2ViewDeclaration) => {
+  $view.viewConfigFactory('ng1-to-ng2', (node: PathNode, config: Ng2ViewDeclaration) => {
     var ng1ViewConfig = new Ng1ViewConfig(<any> node, <any> Object.assign({}, config, { $type: 'ng1'}));
     var ng2ViewConfig = new Ng2ViewConfig(<any> node, <any> Object.assign({}, config, { $type: 'ng2'}));
 
