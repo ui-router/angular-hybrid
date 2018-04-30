6e7ed9a3d91a51dbde11f57324fc7dc53197bbe5
Chris Thielen <cthielen@netflix.com>
2018-04-30 12:26:40 -0700

    test(cypress): use baseUrl for cypress e2e tests

--- example/cypress/integration/example_spec.js
+++ example/cypress/integration/example_spec.js
@@ -1,39 +1,51 @@
 describe('example app', () => {
   it('loads', () => {
-    cy.visit('http://localhost:4000');
+    cy.visit('');
   });
 
   it('renders links', () => {
-    cy.visit('http://localhost:4000/');
+    cy.visit('/');
     cy.get('a').contains('app.ng1.ng2');
     cy.get('a').contains('app.ng2.ng2');
   });
 
   it('renders angularjs', () => {
-    cy.visit('http://localhost:4000');
-    cy.get('a').contains('app.ng1').click();
+    cy.visit('');
+    cy
+      .get('a')
+      .contains('app.ng1')
+      .click();
     cy.url().should('include', '#!/ng1');
     cy.contains('ng1 component');
   });
 
   it('renders angular', () => {
-    cy.visit('http://localhost:4000');
-    cy.get('a').contains('app.ng2').click();
+    cy.visit('');
+    cy
+      .get('a')
+      .contains('app.ng2')
+      .click();
     cy.url().should('include', '#!/ng2');
     cy.contains('ng2 component');
   });
 
   it('renders angular inside angularjs', () => {
-    cy.visit('http://localhost:4000');
-    cy.get('a').contains('app.ng1.ng2').click();
+    cy.visit('');
+    cy
+      .get('a')
+      .contains('app.ng1.ng2')
+      .click();
     cy.url().should('include', '#!/ng1/ng2');
     cy.contains('ng1 component');
     cy.contains('ng2 component');
   });
 
   it('renders angular inside angular', () => {
-    cy.visit('http://localhost:4000');
-    cy.get('a').contains('app.ng2.ng2').click();
+    cy.visit('');
+    cy
+      .get('a')
+      .contains('app.ng2.ng2')
+      .click();
     cy.url().should('include', '#!/ng2/ng2');
     cy.contains('ng2 component');
   });
