08e985b48bd4576deb10d8fbe4d7a840ea75024d
Oscar Busk <oscar.busk@gmail.com>
2018-05-07 12:04:56 +0200

    Add NgHybridStateProvider

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,5 +1,5 @@
 import { Ng2StateDeclaration, StatesModule } from '@uirouter/angular';
-import { Ng1StateTransitionHook } from '@uirouter/angularjs';
+import { Ng1StateTransitionHook, StateProvider, Ng1StateDeclaration } from '@uirouter/angularjs';
 import { IInjectable } from '@uirouter/core';
 
 export interface _NgHybridStateDeclaration extends Ng2StateDeclaration {
@@ -12,6 +12,11 @@ export interface _NgHybridStateModule extends StatesModule {
   states?: any;
 }
 
+export interface _NgHybridStateProvider extends StateProvider {
+  state(name: any, definition: any): _NgHybridStateProvider;
+  state(definition: any): _NgHybridStateProvider;
+}
+
 export interface NgHybridStateDeclaration extends _NgHybridStateDeclaration {
   onEnter?: Ng1StateTransitionHook | IInjectable;
   onRetain?: Ng1StateTransitionHook | IInjectable;
@@ -20,4 +25,9 @@ export interface NgHybridStateDeclaration extends _NgHybridStateDeclaration {
 
 export interface NgHybridStatesModule extends _NgHybridStateModule {
   states?: NgHybridStateDeclaration[];
+}
+
+export interface NgHybridStateProvider extends _NgHybridStateProvider {
+  state(name: string, definition: NgHybridStateDeclaration | Ng1StateDeclaration): NgHybridStateProvider;
+  state(definition: NgHybridStateDeclaration | Ng1StateDeclaration): NgHybridStateProvider;
 }
