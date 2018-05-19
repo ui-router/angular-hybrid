c0e55ada5bd16f943cf41a87d59c9d0f8fd1cdbf
Chris Thielen <cthielen@netflix.com>
2018-05-19 16:11:44 -0700

    fix(typings): Allow stateRegistry.register() and stateProvider.state() to take either Ng1 or Ng2StateDeclaration Closes #147 Closes #148

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,5 +1,6 @@
 import { Ng2StateDeclaration, StatesModule } from '@uirouter/angular';
-import { Ng1StateTransitionHook } from '@uirouter/angularjs';
+import { Ng1StateDeclaration, Ng1StateTransitionHook } from '@uirouter/angularjs';
+import { StateProvider } from '@uirouter/angularjs/lib/stateProvider';
 import { IInjectable } from '@uirouter/core';
 
 export interface _NgHybridStateDeclaration extends Ng2StateDeclaration {
@@ -20,4 +21,20 @@ export interface NgHybridStateDeclaration extends _NgHybridStateDeclaration {
 
 export interface NgHybridStatesModule extends _NgHybridStateModule {
   states?: NgHybridStateDeclaration[];
+}
+
+declare module '@uirouter/core/lib/state/stateRegistry' {
+  interface StateRegistry {
+    register(state: Ng1StateDeclaration);
+    register(state: Ng2StateDeclaration);
+  }
+}
+
+declare module '@uirouter/angularjs/lib/stateProvider' {
+  interface StateProvider {
+    state(name: string, definition: Ng1StateDeclaration): StateProvider;
+    state(name: string, definition: Ng2StateDeclaration): StateProvider;
+    state(definition: Ng1StateDeclaration): StateProvider;
+    state(definition: Ng2StateDeclaration): StateProvider;
+  }
 }
